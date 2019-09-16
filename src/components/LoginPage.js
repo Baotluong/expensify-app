67e2e45a4eed0fc77ac0d76128608f69e7a5cbb8
Bao Luong <baotluong@gmail.com>
2019-09-16 18:18:00 -0400

    styled app

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,8 +3,12 @@ import { startLogin } from '../actions/auth';
 import { connect } from 'react-redux';
 
 export const LoginPage = ({ startLogin }) => (
-    <div>
-        <button onClick={startLogin}>Login!</button>
+    <div className="box-layout">
+        <div className="box-layout__box">
+            <h1>Expensify</h1>
+            <p>It's time to get your expenses in order.</p>
+            <button className="button" onClick={startLogin}>Login with Google</button>
+        </div>
     </div>
 );
 
